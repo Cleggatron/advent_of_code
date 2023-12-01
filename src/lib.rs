ffb61216bffa65c8ee1eb14992ab282c5d409be1
Cleggatron <david.robert.clegg@gmail.com>
2023-12-01 22:55:11 +0000

    refactor to return option rather than panic on error

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,41 @@
-fn extract_calibration(line: &str) -> u32{
+fn extract_calibration(line: &str) -> Option<u32>{
     let mut first_val:Option<u32> = None;
     let mut last_val:Option<u32> = None;
 
     for c in line.chars() {
-        if c.is_digit(10) {
-            match first_val {
-                None => {
-                    first_val = Some(c.to_digit(10).unwrap());
-                    last_val = Some(c.to_digit(10).unwrap());
-                },
-                Some(_) => last_val = Some(c.to_digit(10).unwrap())
-            }
+        match c.to_digit(10) {
+            Some(x) => {
+                if first_val == None {
+                    first_val = Some(x);
+                    last_val = Some(x);
+                } else {
+                    last_val = Some(x);
+                }
+            },
+            None => continue,
+
         }
     }
-    
-    first_val.unwrap() * 10 + last_val.unwrap()
+
+    let formatted = match first_val {
+        Some(x) => Some(first_val.unwrap() * 10 + last_val.unwrap()),
+        None => None
+    };
+
+    formatted
 }
 
 #[test]
 fn it_reads_correctly() {
-    assert_eq!(12, extract_calibration("1abc2"));
+    assert_eq!(12, extract_calibration("1abc2").unwrap());
 }
 
 #[test]
 fn it_handles_multiple_digits(){
-    assert_eq!(12, extract_calibration("a12b3n2m"));
+    assert_eq!(12, extract_calibration("a12b3n2m").unwrap());
 }
 
 #[test]
 fn it_crashes(){
-    extract_calibration("abcdefg");
+    assert_eq!(None,extract_calibration("abcdefg"));
 }
