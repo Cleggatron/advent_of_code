51a1d887daca57ce541cd011034becd2056e2b04
Cleggatron <david.robert.clegg@gmail.com>
2023-12-03 09:49:05 +0000

    file read working

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ use advent_of_code::calculate_calibration;
 
 fn main() {
     // Day One
-    // let calibration_figures = fs::read_to_string("./calibration_figures.txt").expect("Should have been able to read file");
+    // let calibration_figures = fs::read_to_string("./advent_of_code_input_files/calibration_figures.txt").expect("Should have been able to read file");
     // let lines: Vec<&str> = calibration_figures.lines().collect();
     // let total = calculate_calibration(lines);
     // println!("{}", total);
+
+    //Day Two
+
+    let game_figures = fs::read_to_string("./advent_of_code_input_files/game_figures.txt").expect("Unable to read file");
+    println!("{game_figures}");
+    let lines: Vec<&str> = game_figures.lines().collect();
 
 }
 
