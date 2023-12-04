c0d1130a9f44defb9ddcecdec421d7991d2bea41
Cleggatron <david.robert.clegg@gmail.com>
2023-12-04 12:34:28 +0000

    wrote file read for day 4

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,28 @@ use advent_of_code::{calculate_calibration, calculate_ids};
 
 fn main() {
     // Day One
-    // let calibration_figures = fs::read_to_string("./advent_of_code_input_files/calibration_figures.txt").expect("Should have been able to read file");
+    // let calibration_figures = fs::read_to_string("./advent_of_code_input_files/day_1.txt").expect("Should have been able to read file");
     // let lines: Vec<&str> = calibration_figures.lines().collect();
     // let total = calculate_calibration(lines);
     // println!("{}", total);
 
     //Day Two
 
-    let game_figures = fs::read_to_string("./advent_of_code_input_files/game_figures.txt").expect("Unable to read file");
-    let lines: Vec<&str> = game_figures.lines().collect();
-    let (id_total , power_total)= calculate_ids(lines);
-    println!("ID Total: {id_total}\nPower Total: {power_total}");
+    // let game_figures = fs::read_to_string("./advent_of_code_input_files/day_2.txt").expect("Unable to read file");
+    // let lines: Vec<&str> = game_figures.lines().collect();
+    // let (id_total , power_total)= calculate_ids(lines);
+    // println!("ID Total: {id_total}\nPower Total: {power_total}");
+
+
+    //Day Three
+
+    //ToDo
+
+    //Day Four
+
+    let scratchcard_data = fs::read_to_string("./advent_of_code_input_files/day_4.txt").expect("Unable to read file");
+    let lines :Vec<&str> = scratchcard_data.lines().collect();
+    println!("{scratchcard_data}");
 
 }
 
