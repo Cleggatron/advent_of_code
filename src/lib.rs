dd38b58c995ee8fc45f8c95b8a1d6643a414aa9b
Cleggatron <david.robert.clegg@gmail.com>
2023-12-08 23:53:20 +0000

    function wrote for day 6 challenge 1 to parse

--- src/lib.rs
+++ src/lib.rs
@@ -179,8 +179,26 @@ fn calculate_scratchcard_points(line: &str) -> u32{
     if commonalities.len() > 0 {
         return u32::pow(2, (commonalities.len() as u32)-1);
     }
-
     0
 
 }
 
+fn calculate_permutations(time :u32, distance :u32) -> u32 {
+    let mut permutations = 0;
+
+    let mut current_speed :u32= 1;
+    let mut current_time :u32 = time-1;
+    while current_speed < time {
+        if current_speed * current_time > distance{
+            permutations +=1;
+        }
+        current_speed += 1;
+        current_time -= 1;
+    }
+    permutations
+}
+
+#[test]
+fn it_calculates_correctly(){
+    assert_eq!(calculate_permutations(7, 9), 4)
+}
